53628a3960d26c820258b98e1fb5f3c575365452
anton.kryvko <anton.kryvko@gmail.com>
2019-07-26 20:01:32 +0300

    Modified get_candidates.py according to PEP-8, added functions to get trusted persons and registration cancellation; added license and readme

--- get_candidates.py
+++ get_candidates.py
@@ -30,7 +30,8 @@ def get_links_to_districts():
     request = requests.get(START_URL)
     soup = BeautifulSoup(request.content, 'html.parser')
     links = soup.select('td.cntr:nth-of-type(2) > a.a2')
-    districts = [CURRENT_ELECTION_URL + links[idx]['href'] for idx, link in (enumerate(links))]
+    districts = [CURRENT_ELECTION_URL + links[idx]['href']
+                 for idx, link in (enumerate(links))]
     print('Посилання на сторінки округів завантажені')
     return districts
 
@@ -55,13 +56,20 @@ def get_links_to_candidates_on_districts(i, district_url):
     request = requests.get(district_url)
     soup = BeautifulSoup(request.content, 'html.parser')
     links = soup.select('td.cntr:nth-of-type(1) > a.a2')
-    candidates_on_districts = [CURRENT_ELECTION_URL + links[idx]['href'] for idx, link in (enumerate(links))]
-    print('Завантажені посилання на сторінки кандидатів на окрузі №{}'.format(i))
+    candidates_on_districts = [CURRENT_ELECTION_URL + links[idx]['href']
+                               for idx, link in (enumerate(links))]
+    print(
+        'Завантажені посилання на профілі кандидатів на окрузі №{}'.format(i))
     return candidates_on_districts
 
 
 def get_candidate_info(candidate_url):
     """Function gets an ordered dictionary with a candidate's information.
+
+    This is: full name, district, nomination subject, registration status,
+    background, trusted persons and links to program and photo. There are some
+    differences in markdown whether is there registration cancellation or not,
+    which are hold by if-statement and try-except block.
 
     Parameters
     ----------
@@ -71,7 +79,8 @@ def get_candidate_info(candidate_url):
     Returns
     -------
     candidate_info_dict: OrderedDict
-        a dictionary with candidate's page column contents, a link to candidate's photo and a link to candidate's program
+        a dictionary with candidate's page column contents,
+        a link to candidate's photo and a link to candidate's program
 
     """
 
@@ -79,30 +88,83 @@ def get_candidate_info(candidate_url):
     soup = BeautifulSoup(request.content, 'html.parser')
     candidate_info_dict = OrderedDict()
     candidate_info_dict['ПІБ'] = soup.h1.get_text()
-    candidate_info_dict['Округ'] = soup.select('table tr > td:nth-of-type(2)')[0].get_text()
-    candidate_info_dict['Висування'] = soup.select('table tr > td:nth-of-type(2)')[1].get_text()
-    candidate_info_dict['Номер і дата реєстрації'] = soup.select('table tr > td:nth-of-type(2)')[3].get_text()
-    candidate_info_dict['Основні відомості'] = soup.select('table tr > td:nth-of-type(2)')[4].get_text()
-    candidate_info_dict['Передвиборна програма'] = CURRENT_ELECTION_URL + str(soup.select('table tr > td:nth-of-type(2) > a.a1')[0].get('href'))
-    candidate_info_dict['Фотографія'] = CURRENT_ELECTION_URL + str(soup.img['src'])
+    candidate_info_dict['Округ'] = \
+        soup.select('table tr > td:nth-of-type(2)')[0].get_text()
+    candidate_info_dict['Висування'] = \
+        soup.select('table tr > td:nth-of-type(2)')[1].get_text()
+    candidate_info_dict['Номер і дата реєстрації'] = \
+        soup.select('table tr > td:nth-of-type(2)')[3].get_text()
+    if len(soup.find_all('tr')) <= 7:
+        candidate_info_dict[
+            'Номер та дата рішення про скасування реєстрації кандидата'
+            ] = ''
+        candidate_info_dict['Основні відомості'] = \
+            soup.select('table tr > td:nth-of-type(2)')[4].get_text()
+    else:
+        candidate_info_dict[
+            'Номер та дата рішення про скасування реєстрації кандидата'
+            ] = soup.select('table tr > td:nth-of-type(2)')[4].get_text()
+        candidate_info_dict['Основні відомості'] = \
+            soup.select('table tr > td:nth-of-type(2)')[6].get_text()
+    candidate_info_dict['Передвиборна програма'] = CURRENT_ELECTION_URL + \
+        str(soup.select('table tr > td:nth-of-type(2) > a.a1')[0].get('href'))
+    candidate_info_dict['Фотографія'] = CURRENT_ELECTION_URL + \
+        str(soup.img['src'])
+    try:
+        trustees_link = CURRENT_ELECTION_URL + \
+            str(soup.select(
+                'table tr > td:nth-of-type(2) > a.a2'
+                )[0].get('href'))
+    except IndexError:
+        candidate_info_dict['Довірені особи'] = ''
+    else:
+        candidate_info_dict['Довірені особи'] = \
+            get_candidate_trustees(trustees_link)
     print('Завантажена інформація про {}'.format(candidate_info_dict['ПІБ']))
     return candidate_info_dict
 
 
+def get_candidate_trustees(trustees_link):
+    """Function gets a string with candidate's trusted persons.
+
+    Parameters
+    ----------
+    trustees_link: string
+        a link to candidate's trusted persons page
+
+    Returns
+    -------
+    trustees: string
+        a string with candidate's trusted persons
+
+    """
+
+    request = requests.get(trustees_link)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    trustees = ','.join(trustee.get_text() for trustee in soup.find_all(
+        'td', class_='b'))
+    return trustees
+
+
 def download_candidate_photo(photo_url, candidate_info_dict):
     """Function downloads candidate's photo.
+
+    In case there is no catalogue to save photo into, creates one. The name of
+    photo is candidate's name plus file extension.
 
     Parameters
     ----------
     photo_url: string
         a link to candidate's photo, received from candidate_info_dict
     candidate_info_dict: OrderedDict
-        a dictionary with candidate's page column contents, a link to candidate's photo and a link to candidate's program
+        a dictionary with candidate's page column contents,
+        a link to candidate's photo and a link to candidate's program
 
     """
 
     response = requests.get(photo_url)
-    photo_name = candidate_info_dict['ПІБ'].replace(' ', '_') + '.' + photo_url.split('.')[-1]
+    photo_name = candidate_info_dict['ПІБ'].replace(' ', '_') + '.' + \
+        photo_url.split('.')[-1]
     if not os.path.exists(PATH_TO_LOCATION_WITH_PHOTOS):
         os.makedirs(PATH_TO_LOCATION_WITH_PHOTOS)
     with open((PATH_TO_LOCATION_WITH_PHOTOS + photo_name), 'wb') as f:
@@ -113,17 +175,22 @@ def download_candidate_photo(photo_url, candidate_info_dict):
 def download_candidate_program(program_url, candidate_info_dict):
     """Function downloads candidate's program.
 
+    In case there is no catalogue to save program into, creates one. The name
+    of program is candidate's name plus file extension.
+
     Parameters
     ----------
     program_url: string
         a link to candidate's program, received from candidate_info_dict
     candidate_info_dict: OrderedDict
-        a dictionary with candidate's page column contents, a link to candidate's photo and a link to candidate's program
+        a dictionary with candidate's page column contents,
+        a link to candidate's photo and a link to candidate's program
 
     """
 
     response = requests.get(program_url)
-    program_name = candidate_info_dict['ПІБ'].replace(' ', '_') + '.' + program_url.split('.')[-1]
+    program_name = candidate_info_dict['ПІБ'].replace(' ', '_') + '.' + \
+        program_url.split('.')[-1]
     if not os.path.exists(PATH_TO_LOCATION_WITH_PROGRAMS):
         os.makedirs(PATH_TO_LOCATION_WITH_PROGRAMS)
     with open((PATH_TO_LOCATION_WITH_PROGRAMS + program_name), 'wb') as f:
@@ -138,27 +205,31 @@ def get_csv_with_candidates_info(candidates_info_dicts):
     ----------
     candidates_info_dicts: list
         a list with candidate_info_dict OrderedDicts
-"""
 
-    cands_info = pd.concat(candidates_info_dicts, ignore_index = True)
+    """
+
+    cands_info = pd.concat(candidates_info_dicts, ignore_index=True)
     cands_info.index.name = 'id'
     cands_info.to_csv('output.csv')
 
 
 def main():
-    """The main function, which leads workflow.
-
-    """
+    """The main function, which leads workflow."""
 
     candidates_info_dicts = []
     districts = get_links_to_districts()
     for i, district in enumerate(districts, start=11):
-        candidates_on_districts = get_links_to_candidates_on_districts(i, district)
+        candidates_on_districts = \
+            get_links_to_candidates_on_districts(i, district)
         for candidate in candidates_on_districts:
             candidate_info_dict = get_candidate_info(candidate)
-            download_candidate_photo(candidate_info_dict['Фотографія'], candidate_info_dict)
-            download_candidate_program(candidate_info_dict['Передвиборна програма'], candidate_info_dict)
-            candidates_info_dicts.append(pd.DataFrame(candidate_info_dict, index=[0]))
+            download_candidate_photo(candidate_info_dict['Фотографія'],
+                                     candidate_info_dict)
+            download_candidate_program(
+                candidate_info_dict['Передвиборна програма'],
+                candidate_info_dict)
+            candidates_info_dicts.append(pd.DataFrame(candidate_info_dict,
+                                         index=[0]))
     get_csv_with_candidates_info(candidates_info_dicts)
 
 
@@ -167,6 +238,6 @@ if __name__ == '__main__':
     try:
         main()
     except:
-        print('Помилка. Перевірте зʼєднання та правильність написання пошукових селекторів')
+        print('Помилка')
     else:
         print('Завантаження успішне')
